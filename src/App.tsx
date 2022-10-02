dd85bc508dac64a43814f282755f401d3d47c560
chris097 <christian@esettlementgroup.com>
2022-10-02 01:30:48 +0100

    professional summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
+import ProfessionalSummary from './pages/professionalSummary';
 import Resume from './pages/resume';
 
 
 function App() {
   return (
-    <div>
-      <Resume />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Resume />} />
+        <Route path='/professional-summary' element={<ProfessionalSummary />} />
+      </Routes>
+    </Router>
   );
 }
 
