36d92979c06decc250af47a319374c4397947987
chris097 <chrisfidel.international@gmail.com>
2022-10-30 07:46:15 +0100

    setup api connection

---
+++ src/helpers/api/fetchApi.ts
@@ -0,0 +1,16 @@
+const fetchApi = (url:string) => {
+    const headers = new Headers({
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+        // authorization: `Bearer ${token}`,
+    });
+
+    const request = new Request(url, {
+        headers,
+    });
+
+    return fetch(request)
+        .then(response => response.json())
+        .then(data => data)
+}
