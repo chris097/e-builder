fbf038e00e97820be15830bb3db7e2ed2d3d9e1c
chris097 <chrisfidel.international@gmail.com>
2022-11-03 06:33:04 +0100

    connect endpoin

--- src/components/education/index.tsx
+++ src/components/education/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import useQueryApi from '../../helpers/useQuery'
+import dateFormat from '../dateFormat';
 
 const Education = () => {
+  const { data, isLoading } = useQueryApi(['education'], 'user/education');
+  const educations = data?.data;
+  
   return (
     <div className='px-6 py-7 bg-white mb-4 rounded-md h-[164.74px]'>
         <div className='font-medium text-xl mb-5'>Education</div>
-        <div className='flex flex-col space-y-2'>
-        <div className='text-secondarygray text-sm'>B.Sc in Computer Science</div>
-        <div className='text-secondarygray text-sm'>National Open University of Nigeria</div>
-        <div className='text-secondarygray text-sm font-light'>2015 - 2019</div>
+      {isLoading ? " " : educations?.map((school:any, index:any) => (
+        <div key={index} className='flex flex-col space-y-2'>
+          <div className='text-secondarygray text-sm capitalize'>{school.degree} in {school.field}</div>
+          <div className='text-secondarygray text-sm capitalize'>{school.school_name}</div>
+          <div className='text-secondarygray text-sm font-light'>{dateFormat(school.start_date)} - {dateFormat(school.end_date)}</div>
         </div>
+      ))}
     </div>
   )
 }
