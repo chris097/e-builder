c5c893d0c8ab32865664c15cb6c847b4e86dc1d7
chris097 <chrisfidel.international@gmail.com>
2023-02-03 16:30:56 +0100

    fixed deploy issues

--- src/Routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { publicRoutes } from "./publicRoutes";
-import { privateRoutes } from "./privateRoutes";
-
-const routes = {
-    publicRoutes,
-    privateRoutes
-};
-
-export default routes;
